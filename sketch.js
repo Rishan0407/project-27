3d5cabf7503452441a24fc60b781b00e1209f037
Rishan0407 <bansi.gobse@gmail.com>
2020-04-08 12:55:18 +0530

    project27

--- sketch.js
+++ sketch.js
@@ -1,9 +1,94 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options={
+    isStatic : true
+  }
+
+
+  var holder_options={
+    isStatic: true
+  }
+
+  ground = Bodies.rectangle(200,330,800,20,ground_options)
+  World.add(world,ground);
+
+holder = Bodies.rectangle(200,100,200,20,holder_options);
+World.add(world,holder);
+
+var ball_options = {
+
+  restitution : 1.0,
+  density : 1.0
+
+}
+
+ball  = Bodies.circle(220,200,40,ball_options);
+World.add(world,ball);
+
+
+var options = {
+  bodyA : ball,
+  bodyB : holder,
+  stiffness: 0.004,
+  length : 100
+}
+var string = Constraint.create(options);
+World.add(world,string);
+
+fill("White");
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background("pink");  
+   
+  Engine.update(engine);
+
+fill("black");
+  text("Press space bar to oscillate the pendulam with mouse",200,20);
+  text("Press Enter to stop the Pendulum from oscillating",200,70);
+  strokeWeight(9);
+  text("oscillating pendulam",380,250);
+
+  fill ("brown");
+rectMode(CENTER);
+rect(holder.position.x,holder.position.y,200,20);
+
+fill(0);
+rectMode(CENTER);
+rect(ground.position.x,ground.position.y,400,20);
+
+
+fill("red");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,40);
+
+strokeWeight(8);
+stroke("green");
+line(ball.position.x,ball.position.y,holder.position.x,holder.position.y)
+
+
+
+
+if(keyCode===32){
+ball.position.y = mouseY;
+ball.position.x = mouseX;
+}
+
+else if (keyCode === ENTER){
+ball.position.x = 200;
+
+}
+
 }
